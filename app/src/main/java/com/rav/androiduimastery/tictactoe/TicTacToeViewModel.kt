ebd74c6d90070fb152b30bdc6024312feeb8d414
Ravneet Singh <30868824+JustlikeRav@users.noreply.github.com>
2025-05-23 12:25:26 -0400

    base grid ready

--- app/src/main/java/com/rav/androiduimastery/tictactoe/TicTacToeViewModel.kt
+++ app/src/main/java/com/rav/androiduimastery/tictactoe/TicTacToeViewModel.kt
@@ -3,12 +3,27 @@ package com.rav.androiduimastery.tictactoe
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class TicTacToeViewModel : ViewModel() {
     private val _gameState = MutableStateFlow<GameState>(GameState(currentPlayer = Player.P_X))
     val gameState = _gameState.asStateFlow()
 
+    init {
+        val newGrid = listOf(
+            listOf<Cell>(Cell.NULL, Cell.NULL, Cell.NULL),
+            listOf<Cell>(Cell.NULL, Cell.NULL, Cell.NULL),
+            listOf<Cell>(Cell.NULL, Cell.NULL, Cell.NULL),
+        )
+        _gameState.update {
+            GameState(
+                grid = newGrid,
+                currentPlayer = Player.P_X
+            )
+        }
+    }
+
     fun playMove(position: Pair<Int, Int>) {
-        
+
     }
 }
